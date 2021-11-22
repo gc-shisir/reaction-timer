305d54316e2878b9421321c58047875ef860bba9
gc <shisir.gc44@gmail.com>
2021-11-22 21:17:39 +0545

    initial commit

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>The Score is: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score <= 250) {
+      this.rank = "Fast Response";
+    } else if (this.score > 250 && this.score <= 600) {
+      this.rank = "Rapid reflexes";
+    } else {
+      this.rank = "Snail pace";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: teal;
+  font-weight: 700;
+}
+</style>
